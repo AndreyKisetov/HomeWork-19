bdd8a1cdbf0e7e6bacf2a81b029b826d4385cca4
Andrey Kisetov <rbcfff@yandex.ru>
2022-07-07 09:28:07 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorld
+{
+    // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Введите пятизначное положительное число: ");
+            string? number = Console.ReadLine();
+            void CheckingNumber(string number)
+            {
+            if (number[0]==number[4] || number[1]==number[3])
+            {
+                Console.WriteLine($"Ваше число: {number} - палиндром.");
+            }
+            else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+            }
+            if (number!.Length == 5)
+            {
+            CheckingNumber(number);
+            }
+            else Console.WriteLine($"Введено неправильное число!");
+        }
+    }
+}
